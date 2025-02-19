4acb80c32846bae8b453fe6a2adb27626d241d0e
Richardthetechy <techchuks16@gmail.com>
2025-02-19 10:07:26 +0100

    ui changes

--- src/sections/Loading.jsx
+++ src/sections/Loading.jsx
@@ -1,11 +1,11 @@
 import { Skeleton } from "@/components/ui/skeleton"
 const Loading = () => {
     return (
-        <div className="flex flex-col space-y-3">
-            <Skeleton className="h-[225px] w-[350px] rounded-xl" />
+        <div className="flex flex-col space-y-3 w-full max-w-[400px]">
+            <Skeleton className="w-full h-[225px]  rounded-xl" />
             <div className="space-y-2">
-                <Skeleton className="h-4 w-[250px]" />
-                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 " />
+                <Skeleton className="h-4" />
             </div>
         </div>
     )
