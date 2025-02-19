e2e642d5cc9509fdcff1d4cf44bf1a4893203ca5
Richardthetechy <techchuks16@gmail.com>
2025-02-19 02:37:56 +0100

    bug fixing

--- src/models/prompt.js
+++ src/models/prompt.js
@@ -2,7 +2,7 @@ async function Prompt(userPrompt) {
     try {
         const capabilities = await window.ai.languageModel.capabilities();
         if (capabilities.available === "readily") {
-            const session = await self.ai.languageModel.create();
+            const session = await window.ai.languageModel.create();
             const prompt = userPrompt;
             const result = await session.prompt(prompt);
             console.log(result); // Negative
